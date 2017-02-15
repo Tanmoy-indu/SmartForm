f30a1e70199a25764f5599b933c624c9f3e17a7d
Tanmoy-indu <tanmoy433@gmail.com>
2017-02-15 09:39:50 +0600

    Update smartForm.js

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,37 @@
 // Your jQuery goes here
 
 var userData = {};
+$("#start").click(function(event){
+		$("#q1").show();
+		$("#welcome").hide();
+	});
+
+
+$("#q1next").click(function(event){
+		$("#q2").show();
+		$("#q1").hide();
+	});
+
+$("#h1next").click(function(event){
+		$("#q2a").show();
+		$("#q2").hide();
+	});
+$("#h2next").click(function(event){
+		$("#q2b").show();
+		$("#q2").hide();
+	});
+$("#h3next").click(function(event){
+		$("#q2c").show();
+		$("#q2").hide();
+	});
+
+
+$("#btnRight").click(function(event){
+		$('#next').on('click', getNext);
+        $('#prev').on('click', getPrev);
+	});
+
+ $("#btnRight").click(function(event){
+		$("#q2").show();
+		$("#welcome").hide();
+	});
